b69a90a49c11de305e8a19ecd3e4227446f71e84
oleksandrbi <o.bihary@gmail.com>
2019-03-19 21:00:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/instaparse/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.instaparse;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                login(username,password);
+            }
+        });
+    }
+
+    private void login(String username, String password)
+    {
+    }
+}
