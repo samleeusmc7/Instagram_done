f796687bd2d006f58d86bdbcb4a6ef11c4b375f2
Samleeusmc7 <saml777@yahoo.com>
2019-04-02 13:50:06 -0400

    done

--- app/src/main/java/com/example/instaparse/MainActivity.java
+++ app/src/main/java/com/example/instaparse/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity
 
     private void goMainActivity()
     {
-        Intent i = new Intent(this, Main2Activity.class);
+        Intent i = new Intent(MainActivity.this, Main2Activity.class);
         startActivity(i);
         finish();
     }
